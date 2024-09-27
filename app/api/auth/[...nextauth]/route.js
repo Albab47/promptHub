a60c132ec875ce2765f7a9832efd7c4e7450e081
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-27 18:50:07 +0600

    Update: auth handler route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { signIn } from "next-auth/react";
 
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
@@ -12,36 +11,38 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    })
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check if a use already exist
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      // store the user id from Mongodb to session
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
-      
-      // if not create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
-      }
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // check if a use already exist
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        // if not create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(/\s/g,"").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
