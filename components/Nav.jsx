5c7d1b4101b434e0273391b62e256cb9cd9ecf68
Albab47 <115328782+Albab47@users.noreply.github.com>
2024-09-30 18:36:45 +0600

    Update gradient and primary color

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { CircleStackIcon } from '@heroicons/react/24/solid'
 
 const Nav = () => {
   const {data: session} = useSession()
@@ -22,13 +23,8 @@ const Nav = () => {
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
-        <Image
-          src={"/assets/images/logo.svg"}
-          alt="promptHub logo"
-          width={30}
-          height={30}
-          className="object-contain"
-        />
+        
+        <CircleStackIcon className="w-7 text-primary" />
         <p className="logo_text">PromptHub</p>
       </Link>
 
